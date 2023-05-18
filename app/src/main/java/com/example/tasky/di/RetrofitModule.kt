91d81799cdbcac0943ff81754573b8bc9b57d02b
Melvin <melvinalek@gmail.com>
2023-05-18 15:35:57 -0300

    Use moshi instead of gson

--- app/src/main/java/com/example/tasky/di/RetrofitModule.kt
+++ app/src/main/java/com/example/tasky/di/RetrofitModule.kt
@@ -8,7 +8,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -33,7 +33,7 @@ class RetrofitModule {
         Retrofit
             .Builder()
             .baseUrl(BuildConfig.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create())
             .client(client)
             .build()
 }
