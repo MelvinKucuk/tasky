d4a38a0c07037bb7cc777532afe5788f4bc2459e
Melvin <melvinalek@gmail.com>
2023-05-29 14:05:13 -0300

    Create SignUp Screen and Viewmodel with base logicup

--- app/src/main/java/com/example/tasky/authentication/presentation/components/ButtonWithLoader.kt
+++ app/src/main/java/com/example/tasky/authentication/presentation/components/ButtonWithLoader.kt
@@ -1,4 +1,4 @@
-package com.example.tasky.authentication.presentation.login.components
+package com.example.tasky.authentication.presentation.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,15 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.tasky.R
 import com.example.tasky.ui.theme.Black
 
 @Composable
 fun ButtonWithLoader(
+    text: String,
     modifier: Modifier = Modifier,
     isLoading: Boolean,
     onClick: () -> Unit,
@@ -41,7 +40,7 @@ fun ButtonWithLoader(
             )
         } else {
             Text(
-                text = stringResource(R.string.log_in),
+                text = text,
                 fontSize = 16.sp,
                 fontWeight = FontWeight.Bold
             )
--- app/src/main/java/com/example/tasky/authentication/presentation/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/tasky/authentication/presentation/signup/SignUpScreen.kt
@@ -1,15 +1,15 @@
-package com.example.tasky.authentication.presentation.login
+package com.example.tasky.authentication.presentation.signup
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,31 +19,30 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction.Companion
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tasky.R
+import com.example.tasky.authentication.presentation.components.BackButton
 import com.example.tasky.authentication.presentation.components.ButtonWithLoader
 import com.example.tasky.authentication.presentation.components.TaskyPasswordTextField
 import com.example.tasky.authentication.presentation.components.TaskyTextField
-import com.example.tasky.authentication.presentation.login.viewmodel.LoginEvent
-import com.example.tasky.authentication.presentation.login.viewmodel.LoginState
+import com.example.tasky.authentication.presentation.signup.viewmodel.SignUpEvent
+import com.example.tasky.authentication.presentation.signup.viewmodel.SignUpState
 import com.example.tasky.ui.theme.Black
-import com.example.tasky.ui.theme.LightBlue
-import com.example.tasky.ui.theme.LightGray
 import com.example.tasky.ui.theme.TaskyTheme
 
 @Composable
-fun LoginScreen(
-    loginState: LoginState,
-    onLoginEvent: (LoginEvent) -> Unit
+fun SignUpScreen(
+    signUpState: SignUpState,
+    onSignUpEvent: (SignUpEvent) -> Unit
 ) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         containerColor = Black,
-        ) {
+    ) {
         Column(
             Modifier
                 .fillMaxSize()
@@ -52,7 +51,7 @@ fun LoginScreen(
         ) {
             Text(
                 modifier = Modifier.padding(top = 46.dp),
-                text = stringResource(R.string.welcome_back),
+                text = stringResource(R.string.create_your_account),
                 fontSize = 28.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.White
@@ -80,57 +79,53 @@ fun LoginScreen(
                     Spacer(modifier = Modifier.size(50.dp))
 
                     TaskyTextField(
-                        value = loginState.emailValue,
-                        placeholder = stringResource(R.string.email_address),
-                        imeAction = Companion.Next,
-                        isValid = loginState.isValidEmail
+                        value = signUpState.nameValue,
+                        placeholder = stringResource(R.string.name),
+                        imeAction = ImeAction.Next,
+                        isValid = signUpState.isValidName
                     ) { emailValue ->
-                        onLoginEvent(LoginEvent.OnEmailValueChanged(emailValue))
+                        onSignUpEvent(SignUpEvent.OnNameValueChanged(emailValue))
+                    }
+                    Spacer(modifier = Modifier.size(16.dp))
+
+                    TaskyTextField(
+                        value = signUpState.emailValue,
+                        placeholder = stringResource(R.string.email_address),
+                        imeAction = ImeAction.Next,
+                        isValid = signUpState.isValidEmail
+                    ) { emailValue ->
+                        onSignUpEvent(SignUpEvent.OnEmailValueChanged(emailValue))
                     }
                     Spacer(modifier = Modifier.size(16.dp))
 
                     TaskyPasswordTextField(
-                        value = loginState.passwordValue,
+                        value = signUpState.passwordValue,
                         placeholder = stringResource(R.string.password),
-                        showPassword = loginState.showPassword,
+                        showPassword = signUpState.showPassword,
                         keyboardType = KeyboardType.Password,
                         onShowPasswordChange = { showPassword ->
-                            onLoginEvent(LoginEvent.OnShowPasswordValueChanged(showPassword))
+                            onSignUpEvent(SignUpEvent.OnShowPasswordValueChanged(showPassword))
                         }
                     ) { passwordValue ->
-                        onLoginEvent(LoginEvent.OnPasswordValueChanged(passwordValue))
+                        onSignUpEvent(SignUpEvent.OnPasswordValueChanged(passwordValue))
                     }
                     Spacer(modifier = Modifier.size(25.dp))
                     ButtonWithLoader(
-                        text = stringResource(id = R.string.log_in),
-                        isLoading = loginState.isLoading
+                        text = stringResource(R.string.get_started),
+                        isLoading = signUpState.isLoading
                     ) {
-                        onLoginEvent(LoginEvent.OnLoginClicked)
+                        onSignUpEvent(SignUpEvent.OnSignUpClicked)
                     }
                     Spacer(modifier = Modifier.weight(1f))
                     Row(
                         modifier = Modifier
-                            .padding(
-                                bottom = 60.dp,
-                                top = 10.dp,
-                                start = 10.dp,
-                                end = 10.dp
-                            )
-                            .clickable {
-                                onLoginEvent(LoginEvent.OnSignUpClicked)
-                            }
+                            .fillMaxWidth()
+                            .padding(start = 16.dp, bottom = 68.dp),
+                        horizontalArrangement = Arrangement.Start
                     ) {
-                        Text(
-                            text = stringResource(R.string.dont_have_an_account),
-                            color = LightGray,
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                        Spacer(modifier = Modifier.size(4.dp))
-                        Text(
-                            text = stringResource(R.string.sign_up),
-                            color = LightBlue,
-                            style = MaterialTheme.typography.bodySmall
-                        )
+                        BackButton {
+                            onSignUpEvent(SignUpEvent.OnBackClicked)
+                        }
                     }
                 }
             }
@@ -140,11 +135,8 @@ fun LoginScreen(
 
 @Preview
 @Composable
-fun LoginScreenPreview() {
+fun SignUpScreenPreview() {
     TaskyTheme {
-        LoginScreen(
-            loginState = LoginState(),
-            onLoginEvent = {}
-        )
+        SignUpScreen(SignUpState()) {}
     }
 }
