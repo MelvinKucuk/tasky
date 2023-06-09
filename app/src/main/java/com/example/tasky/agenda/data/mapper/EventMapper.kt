fb0b106f9c6416faa1fadad91aa24cb257b20d67
Melvin <melvinalek@gmail.com>
2023-06-09 18:23:13 -0300

    Add model mappers

--- app/src/main/java/com/example/tasky/agenda/data/mapper/EventMapper.kt
+++ app/src/main/java/com/example/tasky/agenda/data/mapper/EventMapper.kt
@@ -1,5 +1,6 @@
 package com.example.tasky.agenda.data.mapper
 
+import com.example.tasky.agenda.data.local.model.EventEntity
 import com.example.tasky.agenda.data.remote.model.EventResponse
 import com.example.tasky.agenda.domain.model.AgendaItem
 
@@ -15,4 +16,28 @@ fun EventResponse.toDomain() =
         isUserEventCreator = isUserEventCreator,
         attendees = attendees.map { it.toDomain() },
         photos = photos.map { it.toDomain() },
+    )
+
+fun EventResponse.toEntity() =
+    EventEntity(
+        id = id,
+        title = title,
+        description = description,
+        from = from,
+        to = to,
+        remindAt = remindAt,
+        host = host,
+        isUserEventCreator = isUserEventCreator,
+    )
+
+fun EventEntity.toDomain() =
+    AgendaItem.Event(
+        id = id,
+        title = title,
+        description = description,
+        from = from,
+        to = to,
+        remindAt = remindAt,
+        host = host,
+        isUserEventCreator = isUserEventCreator,
     )
--- app/src/main/java/com/example/tasky/agenda/data/mapper/TaskMapper.kt
+++ app/src/main/java/com/example/tasky/agenda/data/mapper/TaskMapper.kt
@@ -1,9 +1,30 @@
 package com.example.tasky.agenda.data.mapper
 
+import com.example.tasky.agenda.data.local.model.TaskEntity
 import com.example.tasky.agenda.data.remote.model.TaskResponse
 import com.example.tasky.agenda.domain.model.AgendaItem
 
 fun TaskResponse.toDomain() =
+    AgendaItem.Task(
+        id = id,
+        title = title,
+        description = description,
+        time = time,
+        remindAt = remindAt,
+        isDone = isDone,
+    )
+
+fun TaskResponse.toEntity() =
+    TaskEntity(
+        id = id,
+        title = title,
+        description = description,
+        time = time,
+        remindAt = remindAt,
+        isDone = isDone
+    )
+
+fun TaskEntity.toDomain() =
     AgendaItem.Task(
         id = id,
         title = title,
